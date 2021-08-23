8e4e8a9dd5b159e1316cdd86df5e0b17a8153ef2
Ashley Towner <13824257+ashleytowner@users.noreply.github.com>
2021-08-23 18:03:13 +0930

    Changed diagnostic error symbol

--- nvim_s/.config/nvim/lua/lsp.lua
+++ nvim_s/.config/nvim/lua/lsp.lua
@@ -67,3 +67,13 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
+
+vim.fn.sign_define("LspDiagnosticsSignError",
+    {text = "●", texthl = "LspDiagnosticsSignError"})
+vim.fn.sign_define("LspDiagnosticsSignWarning",
+    {text = "●", texthl = "LspDiagnosticsSignWarning"})
+vim.fn.sign_define("LspDiagnosticsSignInformation",
+    {text = "●", texthl = "LspDiagnosticsSignInformation"})
+vim.fn.sign_define("LspDiagnosticsSignHint",
+    {text = "●", texthl = "LspDiagnosticsSignHint"})
+
