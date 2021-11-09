fd0758ee33ace8183f6a7997513d63cc57f87434
Ashley Towner <13824257+ashleytowner@users.noreply.github.com>
2021-11-09 11:08:49 +1030

    nvim: Added stylelint lsp

--- nvim_s/.config/nvim/lua/lsp.lua
+++ nvim_s/.config/nvim/lua/lsp.lua
@@ -62,7 +62,7 @@ local configuration = {
 
 --{{{ Configure Language Servers
 
-local servers = { 'pyright', 'clangd' }
+local servers = { 'pyright', 'clangd', 'stylelint_lsp' }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup(configuration)
