e2e42f41de045ca16dff348351914d6211e2f1b2
Ashley Towner <13824257+ashleytowner@users.noreply.github.com>
2021-12-02 17:28:41 +1030

    Cleaned up file structure

--- install.sh
+++ install.sh
@@ -1,31 +1,45 @@
 #!/bin/zsh
 
+# Make sure we are in the correct directory
+basedir=$(dirname "$0")
+stow_dirs=(bash ctags fzf git kitty nvim ranger starship taskwarrior tmux zsh)
+
+pushd "$basedir"
+
 # Install homebrew
-if [ $(uname -s) = 'MacOS' ]; then
+if [[ $(uname -s) = 'MacOS' || $(uname -s) = 'Darwin' && $(which brew > /dev/null) -gt 0 ]]; then
+  echo "Homebrew not installed; Installing..."
   which brew > /dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  echo "Homebrew installed; skipping"
 fi
 
 # Install packages
-which brew > /dev/null && brew bundle
-which apt > /dev/null && xargs sudo apt install <Aptfile
-which npm > /dev/null && xargs -I{} "npm list -g {} || npm i -g {}" <Npmfile
+which brew > /dev/null && echo "Installing homebrew packages..."; brew bundle
+which apt > /dev/null && echo "Installing apt packages..."; xargs sudo apt install <Aptfile
+which npm > /dev/null && echo "Installing npm packages..."; xargs -I{} npm i -g {} <Npmfile
 
 # Install submodules
 git submodule update --init
 
 # Install vimplug
-# sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-#      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-
-# Install packer
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+echo "Installing vimplug..."
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # Install starship
+echo "Installing starship..."
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 
 # Stow everything that needs stowing
-stow *_s
+for d in $stow_dirs
+do
+  echo "Stowing ${d}..."
+  stow $d
+done
 
 # Source zshrc
-source ./zsh_s/.config/zsh/.zshrc
+echo "Sourcing zshrc"
+source ~/.config/zsh/.zshrc
+
+popd
