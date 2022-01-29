4326a16233f094f7d83882ce33393535cafda908
Ashley Towner <13824257+ashleytowner@users.noreply.github.com>
2022-01-29 19:43:07 +1030

    nvim: Added all packages to bootstrap step of Packer

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,5 +1,10 @@
-require('bootstrapPacker')
-require('packer').startup(function(use)
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+return require('packer').startup(function(use)
 
     use 'wbthomason/packer.nvim'
 
@@ -113,4 +118,9 @@ require('packer').startup(function(use)
           config = function() vim.cmd('colorscheme tokyonight') end
         }
 
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
