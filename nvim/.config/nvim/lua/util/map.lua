d0128c8c98b598a43ba134271cb911c55a41073f
Ashley Towner <13824257+ashleytowner@users.noreply.github.com>
2022-01-29 23:45:57 +1030

    nvim: Made LSP keybinds specific to the buffer

--- nvim/.config/nvim/lua/util/map.lua
+++ nvim/.config/nvim/lua/util/map.lua
@@ -1,4 +1,4 @@
-function create_keymap(mode, lhs, rhs, noremap, silent)
+local function create_keymap(mode, lhs, rhs, noremap, silent, buffer)
     local options = {}
     if noremap then
         options.noremap = true
@@ -8,45 +8,50 @@ function create_keymap(mode, lhs, rhs, noremap, silent)
         options.silent = true
     end
 
+    if buffer then
+      vim.api.nvim_buf_set_keymap(0, mode, lhs, rhs, options)
+      return
+    end
+
     vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-function map(binding, mapping, silent)
-  create_keymap('', binding, mapping, false, silent)
+function map(binding, mapping, silent, buffer)
+  create_keymap('', binding, mapping, false, silent, buffer)
 end
 
-function noremap(binding, mapping, silent)
-  create_keymap('', binding, mapping, true, silent)
+function noremap(binding, mapping, silent, buffer)
+  create_keymap('', binding, mapping, true, silent, buffer)
 end
 
-function nmap(binding, mapping, silent)
-  create_keymap('n', binding, mapping, false, silent)
+function nmap(binding, mapping, silent, buffer)
+  create_keymap('n', binding, mapping, false, silent, buffer)
 end
 
-function nnoremap(binding, mapping, silent)
-  create_keymap('n', binding, mapping, true, silent)
+function nnoremap(binding, mapping, silent, buffer)
+  create_keymap('n', binding, mapping, true, silent, buffer)
 end
 
-function vmap(binding, mapping, silent)
-  create_keymap('v', binding, mapping, false, silent)
+function vmap(binding, mapping, silent, buffer)
+  create_keymap('v', binding, mapping, false, silent, buffer)
 end
 
-function vnoremap(binding, mapping, silent)
-  create_keymap('v', binding, mapping, true, silent);
+function vnoremap(binding, mapping, silent, buffer)
+  create_keymap('v', binding, mapping, true, silent, buffer)
 end
 
-function omap(binding, mapping, silent)
-  create_keymap('o', binding, mapping, false, silent)
+function omap(binding, mapping, silent, buffer)
+  create_keymap('o', binding, mapping, false, silent, buffer)
 end
 
-function onoremap(binding, mapping, silent)
-  create_keymap('o', binding, mapping, true, silent);
+function onoremap(binding, mapping, silent, buffer)
+  create_keymap('o', binding, mapping, true, silent, buffer)
 end
 
-function xmap(binding, mapping, silent)
-  create_keymap('x', binding, mapping, false, silent)
+function xmap(binding, mapping, silent, buffer)
+  create_keymap('x', binding, mapping, false, silent, buffer)
 end
 
-function xnoremap(binding, mapping, silent)
-  create_keymap('x', binding, mapping, true, silent);
+function xnoremap(binding, mapping, silent, buffer)
+  create_keymap('x', binding, mapping, true, silent, buffer)
 end
