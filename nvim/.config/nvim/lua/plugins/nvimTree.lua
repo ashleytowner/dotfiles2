5743f98f75f732cfd1d16451e9bc34d2989f657e
Ashley Towner <13824257+ashleytowner@users.noreply.github.com>
2022-01-29 23:46:50 +1030

    nvim: Changed icons in nvimtree

--- nvim/.config/nvim/lua/plugins/nvimTree.lua
+++ nvim/.config/nvim/lua/plugins/nvimTree.lua
@@ -1,6 +1,12 @@
 require('util.map')
 
 vim.g.nvim_tree_quit_on_open = 1
+vim.g.nvim_tree_show_icons = {
+  git = 0,
+  folders = 1,
+  files = 1,
+  folder_arrows = 0
+}
 
 require('nvim-tree').setup({})
 nmap('<leader><leader>', '<cmd>NvimTreeFindFileToggle<cr>', true)
