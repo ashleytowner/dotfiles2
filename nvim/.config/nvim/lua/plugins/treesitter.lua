6b67cb25d663b4647187f6a82dc9b07c5a0abea6
Ashley Towner <13824257+ashleytowner@users.noreply.github.com>
2022-02-25 10:19:10 +1030

    nvim: Enabled treesitter indentation

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,17 +2,20 @@ local parser_config = require ('nvim-treesitter.parsers').get_parser_configs()
 parser_config.typescript.filetype_to_parsername = 'javascript.jsx'
 
 require('nvim-treesitter.configs').setup {
-		ensure_installed = 'maintained', -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-		highlight = {
-				enable = true
-		},
-		incremental_selection = {
-				enable = true,
-				keymaps = {
-						init_selection = 'gnn',
-						node_incremental = 'grn',
-						scope_incremental = 'grc',
-						node_decremental = 'grm',
-				}
-		}
+  ensure_installed = 'maintained', -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  highlight = {
+    enable = true
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = 'gnn',
+      node_incremental = 'grn',
+      scope_incremental = 'grc',
+      node_decremental = 'grm',
+    }
+  },
+  indent = {
+    enable = true
+  }
 }
