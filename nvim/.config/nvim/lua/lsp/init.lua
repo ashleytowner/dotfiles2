2586e59ae558ba482856d3553953a81b175f6384
Ashley Towner <13824257+ashleytowner@users.noreply.github.com>
2022-03-09 15:13:01 +1030

    nvim: Remove lsp signs

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -113,4 +113,20 @@ vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
  }
 )
 
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+ vim.lsp.diagnostic.on_publish_diagnostics, {
+   -- Enable underline, use default values
+   underline = true,
+   -- Enable virtual text, override spacing to 4
+   virtual_text = {
+     spacing = 1,
+   },
+   -- Use a function to dynamically turn signs off
+   -- and on, using buffer local variables
+   signs = function() end,
+   -- Disable a feature
+   update_in_insert = false,
+ }
+)
+
 require('lsp.diagnostics')
