1d1f5808722aaecf5cff1c502702943295c034a3
Ashley Towner <13824257+ashleytowner@users.noreply.github.com>
2022-08-04 20:02:05 +0930

    nvim: Added hop command

--- nvim/.config/nvim/lua/plugins/hop.lua
+++ nvim/.config/nvim/lua/plugins/hop.lua
@@ -4,3 +4,4 @@ require('hop').setup()
 
 Noremap('<leader>w', '<cmd>lua require(\'hop\').hint_words({ multi_windows = true })<CR>')
 Noremap('<leader>f', '<cmd>lua require(\'hop\').hint_char1()<CR>')
+Noremap('<leader>s', '<cmd>lua require(\'hop\').hint_char2()<cr>')
