f0dcbca87851e4315344cce0408dc366b1b080fc
Ashley Towner <git@ashtowner.com>
2023-01-10 11:16:54 +1030

    nvim: Stopped YamlValue and JsonValue being available in inappropriate buffers

--- nvim/.config/nvim/ftplugin/json.lua
+++ nvim/.config/nvim/ftplugin/json.lua
@@ -3,6 +3,25 @@ local function json_value(parameter)
 	vim.cmd('!jq \'' .. parameter .. '\' < ' .. filename)
 end
 
-vim.api.nvim_create_user_command('JsonValue', function(input)
-	json_value(input.args)
-end, { nargs=1 });
+local json_value_group = vim.api.nvim_create_augroup(
+	'JsonValueCommand',
+	{ clear = true }
+);
+
+vim.api.nvim_create_autocmd({'BufLeave'}, {
+	pattern = {"*.json"},
+	group = json_value_group,
+	callback = function()
+		pcall(vim.api.nvim_del_user_command, 'JsonValue');
+	end
+});
+
+vim.api.nvim_create_autocmd({'BufEnter'}, {
+	pattern = {"*.json"},
+	group = json_value_group,
+	callback = function()
+		pcall(vim.api.nvim_create_user_command, 'JsonValue', function(input)
+			json_value(input.args)
+		end, { nargs = 1 });
+	end
+});
--- nvim/.config/nvim/ftplugin/yaml.lua
+++ nvim/.config/nvim/ftplugin/yaml.lua
@@ -3,6 +3,25 @@ local function yaml_value(parameter)
 	vim.cmd('!yq \'' .. parameter .. '\' < ' .. filename)
 end
 
-vim.api.nvim_create_user_command('YamlValue', function(input)
-	yaml_value(input.args)
-end, { nargs=1 });
+local yaml_value_group = vim.api.nvim_create_augroup(
+	'YamlValueCommand',
+	{ clear = true }
+);
+
+vim.api.nvim_create_autocmd({'BufLeave'}, {
+	pattern = {'*.yaml', '*.yml'},
+	group = yaml_value_group,
+	callback = function()
+		pcall(vim.api.nvim_del_user_command, 'YamlValue');
+	end
+});
+
+vim.api.nvim_create_autocmd({'BufEnter'}, {
+	pattern = {'*.yaml', '*.yml'},
+	group = yaml_value_group,
+	callback = function()
+		pcall(vim.api.nvim_create_user_command, 'JsonValue', function(input)
+			yaml_value(input.args)
+		end, { nargs = 1 });
+	end
+});
