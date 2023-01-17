c713bbafb982d6bf4a02ba1031bca38fd71ade99
Ashley Towner <git@ashtowner.com>
2023-01-17 14:22:39 +1030

    nvim: removed unused spread function from util

--- nvim/.config/nvim/lua/util/init.lua
+++ nvim/.config/nvim/lua/util/init.lua
@@ -2,20 +2,6 @@ require('util.objects')
 
 local M = {}
 
-function M.spread(template)
-	local result = {}
-	for key, value in pairs(template) do
-		result[key] = value
-	end
-
-	return function(table)
-		for key, value in pairs(table) do
-			result[key] = value
-		end
-		return result
-	end
-end
-
 function M.trim(s)
 	return (s:gsub("^%s*(.-)%s*$", "%1"))
 end
