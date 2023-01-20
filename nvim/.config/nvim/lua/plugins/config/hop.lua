97673e585fcdcd29638ec2e28f626c303da7bda4
Ashley Towner <git@ashtowner.com>
2023-01-20 15:00:25 +1030

    nvim: Changed mapping keys for hop, as well as the jumping keys

--- nvim/.config/nvim/lua/plugins/config/hop.lua
+++ nvim/.config/nvim/lua/plugins/config/hop.lua
@@ -1,26 +1,33 @@
-require('hop').setup()
+require('hop').setup({
+	reverse_distribution = true,
+	multi_windows = true,
+	keys = 'asdfghjkl;qwertyuiop'
+})
 
 vim.keymap.set(
 	'n',
 	'<leader>w',
-	function() require('hop').hint_words({ multi_windows = true }) end,
+	function() require('hop').hint_words() end,
 	{ noremap = true }
 )
+
 vim.keymap.set(
 	'n',
 	'<leader>f',
 	function() require('hop').hint_char1() end,
 	{ noremap = true }
 )
+
 vim.keymap.set(
 	'n',
 	'<leader>s',
 	function() require('hop').hint_char2() end,
 	{ noremap = true }
 )
+
 vim.keymap.set(
 	'n',
-	'<leader>h',
+	'<leader>F',
 	function() require('hop').hint_anywhere() end,
 	{ noremap = true }
 )
