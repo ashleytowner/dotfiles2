cd49198b5773bbcf063dec4a88268ccd29c89292
Ashley Towner <git@ashtowner.com>
2023-01-24 14:39:11 +1030

    nvim: Refactored config for twoslashQueries

--- nvim/.config/nvim/lua/lsp/init.lua
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -4,7 +4,14 @@ local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, "lua/?.lua")
 table.insert(runtime_path, "lua/?/init.lua")
 
-require('mason-lspconfig').setup({
+local masonLspOk, masonLsp = pcall(require, 'mason-lspconfig');
+local lspConfigOk, lspConfig = pcall(require, 'lspconfig');
+
+if not (masonLspOk or lspConfigOk) then
+	return
+end
+
+masonLsp.setup({
 	ensure_installed = {
 		'bashls',
 		'clangd',
@@ -18,38 +25,42 @@ require('mason-lspconfig').setup({
 	}
 })
 
-require('mason-lspconfig').setup_handlers {
+masonLsp.setup_handlers {
 	-- The first entry (without a key) will be the default handler
 	-- and will be called for each installed server that doesn't have
 	-- a dedicated handler.
 	function(server_name)
-		require('lspconfig')[server_name].setup({
+		lspConfig[server_name].setup({
 			on_attach = function()
 				keymaps.set_keymaps()
 			end,
 		})
 	end,
-	['tsserver'] = function()
-		require('lspconfig')['tsserver'].setup({
+	['tsserver'] = function(server_name)
+		lspConfig[server_name].setup({
 			on_attach = function(client, bufnr)
 				keymaps.set_keymaps()
-				local twoslashOk, twoslash = pcall(require, 'twoslash-queries')
-				if twoslashOk then
-					twoslash.attach(client, bufnr)
-					twoslash.enable()
-					vim.keymap.set(
-						'n',
-						'<leader>?',
-						'<cmd>InspectTwoslashQueries<cr>',
-						{ noremap = true, buffer = true }
-					);
-					vim.keymap.set(
-						'n',
-						'<leader><bs>?',
-						'<cmd>RemoveTwoslashQueries<cr>',
-						{ noremap = true, buffer = true }
-					)
+
+				local twoslashQueriesOk, twoslashQueries = pcall(require, 'twoslash-queries');
+				if not twoslashQueriesOk then
+					return;
 				end
+				twoslashQueries.attach(client, bufnr);
+				twoslashQueries.enable();
+
+				vim.keymap.set(
+					'n',
+					'<leader>?',
+					'<cmd>InspectTwoslashQueries<cr>',
+					{ noremap = true, buffer = true }
+				);
+
+				vim.keymap.set(
+					'n',
+					'<leader><bs>?',
+					'<cmd>RemoveTwoslashQueries<cr>',
+					{ noremap = true, buffer = true }
+				);
 			end
 		})
 	end
