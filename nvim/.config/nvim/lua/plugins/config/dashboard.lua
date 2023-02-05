b76e26857f643a08d220244b4e4d943b69b73e43
Ashley Towner <git@ashtowner.com>
2023-02-06 09:37:47 +1030

    nvim: Reconfigured nvim dashboard

--- nvim/.config/nvim/lua/plugins/config/dashboard.lua
+++ nvim/.config/nvim/lua/plugins/config/dashboard.lua
@@ -1,62 +1,80 @@
-local dashboard = require('dashboard')
-dashboard.custom_header = {
-	'',
-	" _   _         __     ___           ",
-	"| \\ | | ___  __\\ \\   / (_)_ __ ___  ",
-	"|  \\| |/ _ \\/ _ \\ \\ / /| | '_ ` _ \\ ",
-	"| |\\  |  __/ (_) \\ V / | | | | | | |",
-	"|_| \\_|\\___|\\___/ \\_/  |_|_| |_| |_|",
-	'                                    ',
-	'',
-	'',
-}
-dashboard.custom_center = {
-	{
-		icon = '  ',
-		desc = 'Find File                              ',
-		action = 'Telescope find_files',
-		shortcut = '<C-p>'
-	},
-	{
-		icon = '  ',
-		desc = 'Browse Files                ',
-		action = 'NvimTreeToggle',
-		shortcut = '<leader><leader>'
-	},
-	{
-		icon = '  ',
-		desc = 'Live Grep                         ',
-		action = 'Telescope live_grep',
-		shortcut = '<leader>t/'
-	},
-	{
-		icon = '祥 ',
-		desc = 'Startup Time                    ',
-		action = 'StartupTime --tries 50',
-		shortcut = ':StartupTime'
-	},
-	{
-		icon = '  ',
-		desc = 'Change Colorscheme                ',
-		action = 'Telescope colorscheme',
-		shortcut = '<leader>tc'
-	},
-	{
-		icon = '  ',
-		desc = 'Check Health                    ',
-		action = 'CheckHealth',
-		shortcut = ':CheckHealth'
-	},
-	{
-		icon = '  ',
-		desc = 'Open Dotfiles                  ',
-		action = 'e ~/dotfiles',
-		shortcut = ':e ~/dotfiles'
-	},
-}
-local version = require('util').trim(require('util').system('nvim --version | grep -o "v\\d\\+\\.\\d\\+\\.\\d\\+"'))
-dashboard.custom_footer = {
-	version
-}
-dashboard.hide_tabline = false
-dashboard.hide_statusline = false
+local version = require('util').trim(require('util').system('nvim --version | grep -o "v[0-9]\\+\\.[0-9]\\+\\.[0-9]"'))
+require('dashboard').setup({
+	theme = 'doom',
+	config = {
+		header = {
+			" _   _         __     ___           ",
+			"| \\ | | ___  __\\ \\   / (_)_ __ ___  ",
+			"|  \\| |/ _ \\/ _ \\ \\ / /| | '_ ` _ \\ ",
+			"| |\\  |  __/ (_) \\ V / | | | | | | |",
+			"|_| \\_|\\___|\\___/ \\_/  |_|_| |_| |_|",
+			''
+		},
+		footer = { version },
+		center = {
+			{
+				icon = '  ',
+				desc = 'Find File',
+				action = 'Telescope find_files',
+				shortcut = '<C-p>',
+				key = '<C-p>',
+				icon_hl = 'Special',
+				key_hl = 'Special'
+			},
+			{
+				icon = '  ',
+				desc = 'Browse Files',
+				action = 'NvimTreeToggle',
+				shortcut = '<leader><leader>',
+				key = '<leader><leader>',
+				icon_hl = 'Special',
+				key_hl = 'Special'
+			},
+			{
+				icon = '  ',
+				desc = 'Live Grep',
+				action = 'Telescope live_grep',
+				shortcut = '<leader>t/',
+				key = '<leader>t/',
+				icon_hl = 'Special',
+				key_hl = 'Special'
+			},
+			{
+				icon = '祥 ',
+				desc = 'Startup Time',
+				action = 'StartupTime --tries 50',
+				shortcut = ':StartupTime',
+				key = ':StartupTime',
+				icon_hl = 'Special',
+				key_hl = 'Special'
+			},
+			{
+				icon = '  ',
+				desc = 'Change Colorscheme',
+				action = 'Telescope colorscheme',
+				shortcut = '<leader>tc',
+				key = '<leader>tc',
+				icon_hl = 'Special',
+				key_hl = 'Special'
+			},
+			{
+				icon = '  ',
+				desc = 'Check Health',
+				action = 'CheckHealth',
+				shortcut = ':CheckHealth',
+				key = ':CheckHealth',
+				icon_hl = 'Special',
+				key_hl = 'Special'
+			},
+			{
+				icon = '  ',
+				desc = 'Open Dotfiles',
+				action = 'e ~/dotfiles',
+				shortcut = ':e ~/dotfiles',
+				key = ':e ~/dotfiles',
+				icon_hl = 'Special',
+				key_hl = 'Special'
+			},
+		}
+	}
+})
