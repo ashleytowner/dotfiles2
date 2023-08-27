125d62e9892a4614b8e1931a583318b018541629
Ashley Towner <git@ashtowner.com>
2023-08-27 15:09:37 +0930

    nvim: Adjusted which treesitter parsers to install

--- nvim/.config/nvim/lua/plugins/config/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/config/treesitter.lua
@@ -2,7 +2,19 @@ local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
 parser_config.typescript.filetype_to_parsername = 'javascript.jsx'
 
 require('nvim-treesitter.configs').setup({
-	ensure_installed = 'all', -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	-- ensure_installed = 'all', -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	ensure_installed = {
+		'javascript',
+		'typescript',
+		'c',
+		'cpp',
+		'gitcommit',
+		'lua',
+		'yaml',
+		'json',
+		'toml',
+		'markdown'
+	},
 	highlight = {
 		enable = true,
 	},
