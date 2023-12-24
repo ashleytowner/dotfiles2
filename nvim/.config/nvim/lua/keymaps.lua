c653a7d1f7ea4dbceac04f6bcfc642de04c61b60
Ashley Towner <git@ashtowner.com>
2023-12-25 00:18:39 +1030

    nvim: Added descriptions to keymaps

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,41 +1,141 @@
 -- Text manipulation
-vim.keymap.set('n', 'J', 'mzJ`z', { noremap = true })
-vim.keymap.set('n', 'N', 'Nzz', { noremap = true })
-vim.keymap.set('n', 'n', 'nzz', { noremap = true })
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { noremap = true })
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { noremap = true })
+vim.keymap.set(
+	'n',
+	'J',
+	'mzJ`z',
+	{ noremap = true, desc = 'Join lines without messing up cursor position' }
+)
+vim.keymap.set(
+	'n',
+	'N',
+	'Nzz',
+	{ noremap = true, desc = 'Go to previous search result and center it' }
+)
+vim.keymap.set(
+	'n',
+	'n',
+	'nzz',
+	{ noremap = true, desc = 'Go to next search result and center it' }
+)
+vim.keymap.set(
+	'v',
+	'J',
+	":m '>+1<CR>gv=gv",
+	{ noremap = true, desc = 'Move selected text down 1 line' }
+)
+vim.keymap.set(
+	'v',
+	'K',
+	":m '<-2<CR>gv=gv",
+	{ noremap = true, desc = 'Move selected text up 1 line' }
+)
 
 -- Clipboard
-vim.keymap.set('', '<leader>P', '"+P', { noremap = true })
-vim.keymap.set('', '<leader>Y', '"+y$', { noremap = true })
-vim.keymap.set('', '<leader>p', '"+p', { noremap = true })
-vim.keymap.set('', '<leader>y', '"+y', { noremap = true })
+vim.keymap.set(
+	'',
+	'<leader>P',
+	'"+P',
+	{ noremap = true, desc = 'Paste from system clipboard' }
+)
+vim.keymap.set(
+	'',
+	'<leader>Y',
+	'"+y$',
+	{ noremap = true, desc = 'Yank to system clipboard' }
+)
+vim.keymap.set(
+	'',
+	'<leader>p',
+	'"+p',
+	{ noremap = true, desc = 'Paste from system clipboard' }
+)
+vim.keymap.set(
+	'',
+	'<leader>y',
+	'"+y',
+	{ noremap = true, desc = 'Yank to system clipboard' }
+)
 
 -- File Navigation
-vim.keymap.set('n', '[q', '<cmd>cprev<cr>')
-vim.keymap.set('n', ']q', '<cmd>cnext<cr>')
-vim.keymap.set('n', '<C-PageDown>', ':bnext<CR>', { noremap = true })
-vim.keymap.set('n', '<C-PageUp>', ':bprev<CR>', { noremap = true })
-vim.keymap.set('n', 'gw', '<C-w>w', { noremap = true })
-vim.keymap.set('n', 'gW', '<C-w>W', { noremap = true })
-vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true })
-vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true })
-vim.keymap.set('n', '<C-6>', '<C-^>', { noremap = true })
+vim.keymap.set('n', '[q', '<cmd>cprev<cr>', { desc = 'Previous quickfix item' })
+vim.keymap.set('n', ']q', '<cmd>cnext<cr>', { desc = 'Next quickfix item' })
+vim.keymap.set(
+	'n',
+	'<C-PageDown>',
+	':bnext<CR>',
+	{ noremap = true, desc = 'Next buffer' }
+)
+vim.keymap.set(
+	'n',
+	'<C-PageUp>',
+	':bprev<CR>',
+	{ noremap = true, desc = 'Previous buffer' }
+)
+vim.keymap.set('n', 'gw', '<C-w>w', { noremap = true, desc = 'Next window' })
+vim.keymap.set(
+	'n',
+	'gW',
+	'<C-w>W',
+	{ noremap = true, desc = 'Previous window' }
+)
+vim.keymap.set(
+	'n',
+	'<C-u>',
+	'<C-u>zz',
+	{ noremap = true, desc = 'Scroll up and center' }
+)
+vim.keymap.set(
+	'n',
+	'<C-d>',
+	'<C-d>zz',
+	{ noremap = true, desc = 'Scroll down and center' }
+)
+vim.keymap.set(
+	'n',
+	'<C-6>',
+	'<C-^>',
+	{ noremap = true, desc = 'Switch to previous buffer' }
+)
 
 -- Terminal
-vim.keymap.set('t', '<C-\\>', '<C-\\><C-n><C-w>w', { noremap = true })
-vim.keymap.set('t', '<C-n>', '<C-\\><C-n>', { noremap = true })
+vim.keymap.set(
+	't',
+	'<C-\\>',
+	'<C-\\><C-n><C-w>w',
+	{ noremap = true, desc = 'Next window out of terminal' }
+)
+vim.keymap.set(
+	't',
+	'<C-n>',
+	'<C-\\><C-n>',
+	{ noremap = true, desc = 'Exit terminal mode' }
+)
 
 -- Diff commands
-if (vim.wo.diff) then
-	vim.keymap.set('n', '<leader>1', ':diffget 1<CR>', { noremap = true })
-	vim.keymap.set('n', '<leader>2', ':diffget 2<CR>', { noremap = true })
-	vim.keymap.set('n', '<leader>3', ':diffget 3<CR>', { noremap = true })
+if vim.wo.diff then
+	vim.keymap.set(
+		'n',
+		'<leader>1',
+		':diffget 1<CR>',
+		{ noremap = true, desc = 'Get diff from left' }
+	)
+	vim.keymap.set(
+		'n',
+		'<leader>2',
+		':diffget 2<CR>',
+		{ noremap = true, desc = 'Get diff from middle' }
+	)
+	vim.keymap.set(
+		'n',
+		'<leader>3',
+		':diffget 3<CR>',
+		{ noremap = true, desc = 'Get diff from right' }
+	)
 end
 
-vim.keymap.set('', '<leader>/', function ()
+vim.keymap.set('', '<leader>/', function()
 	local bufs = require('util').get_buffers()
-	local terminal_buffer = -1;
+	local terminal_buffer = -1
 
 	for _, buf in ipairs(bufs) do
 		if vim.api.nvim_buf_get_option(buf.number, 'buftype') == 'terminal' then
@@ -62,5 +162,4 @@ vim.keymap.set('', '<leader>/', function ()
 			vim.cmd('sbuffer ' .. terminal_buffer)
 		end
 	end
-
-end, { noremap = true })
+end, { noremap = true, desc = 'Open terminal' })
--- nvim/.config/nvim/lua/lsp/keymaps.lua
+++ nvim/.config/nvim/lua/lsp/keymaps.lua
@@ -7,73 +7,115 @@ function P.set_keymaps()
 		{ 'n', 'i' },
 		'<C-k>',
 		vim.lsp.buf.signature_help,
-		{ noremap = true, silent = true, buffer = true }
+		{
+			noremap = true,
+			silent = true,
+			buffer = true,
+			desc = 'Signature help',
+		}
 	)
 
 	vim.keymap.set(
 		'n',
 		'K',
 		vim.lsp.buf.hover,
-		{ noremap = true, silent = true, buffer = true }
+		{ noremap = true, silent = true, buffer = true, desc = 'Hover' }
 	)
 
 	vim.keymap.set(
 		'n',
 		'<leader>.',
 		vim.lsp.buf.code_action,
-		{ noremap = true, silent = true, buffer = true }
+		{ noremap = true, silent = true, buffer = true, desc = 'Code action' }
 	)
 
 	vim.keymap.set(
 		'n',
 		']t',
 		function()
-			if (tscpOk) then
+			if tscpOk then
 				tscp.lsp_type_definitions()
 			else
 				vim.lsp.buf.type_definition()
 			end
 		end,
-		{ noremap = true, silent = true, buffer = true }
+		{
+			noremap = true,
+			silent = true,
+			buffer = true,
+			desc = 'Go to type definition',
+		}
 	)
 
 	vim.keymap.set(
 		'n',
 		'[d',
 		vim.diagnostic.goto_prev,
-		{ noremap = true, silent = true, buffer = true }
+		{
+			noremap = true,
+			silent = true,
+			buffer = true,
+			desc = 'Go to previous diagnostic',
+		}
 	)
 
 	vim.keymap.set(
 		'n',
 		']d',
 		vim.diagnostic.goto_next,
-		{ noremap = true, silent = true, buffer = true }
+		{
+			noremap = true,
+			silent = true,
+			buffer = true,
+			desc = 'Go to next diagnostic',
+		}
 	)
 
-	vim.keymap.set('n', '<leader>d', function()
-		local formatFunction = function(d)
-			return '[' .. d.source .. '] '
-		end
-		vim.diagnostic.open_float(
-			nil,
-			{ prefix = formatFunction }
-		)
-	end, { noremap = true, silent = true, buffer = true })
+	vim.keymap.set(
+		'n',
+		'<leader>d',
+		function()
+			local formatFunction = function(d)
+				return '[' .. d.source .. '] '
+			end
+			vim.diagnostic.open_float(nil, { prefix = formatFunction })
+		end,
+		{
+			noremap = true,
+			silent = true,
+			buffer = true,
+			desc = 'Show diagnostics',
+		}
+	)
 
-	vim.keymap.set('n', '<leader>D', function()
-		if tscpOk then
-			tscp.diagnostics()
-		else
-			vim.diagnostic.setqflist()
-		end
-	end, { noremap = true, silent = true, buffer = true })
+	vim.keymap.set(
+		'n',
+		'<leader>D',
+		function()
+			if tscpOk then
+				tscp.diagnostics()
+			else
+				vim.diagnostic.setqflist()
+			end
+		end,
+		{
+			noremap = true,
+			silent = true,
+			buffer = true,
+			desc = 'Show diagnostics in quickfix list',
+		}
+	)
 
 	vim.keymap.set(
 		'n',
 		']i',
 		vim.lsp.buf.implementation,
-		{ noremap = true, silent = true, buffer = true }
+		{
+			noremap = true,
+			silent = true,
+			buffer = true,
+			desc = 'Go to implementation',
+		}
 	)
 
 	vim.keymap.set('n', '==', function()
@@ -85,65 +127,110 @@ function P.set_keymaps()
 				['end'] = { line, 10000 },
 			},
 		})
-	end, { noremap = true, silent = true, buffer = true })
+	end, { noremap = true, silent = true, buffer = true, desc = 'Format line' })
 
 	vim.keymap.set(
 		'n',
 		'<leader>gi',
 		vim.lsp.buf.implementation,
-		{ noremap = true, silent = true, buffer = true }
+		{
+			noremap = true,
+			silent = true,
+			buffer = true,
+			desc = 'Go to implementation',
+		}
 	)
 
 	vim.keymap.set(
 		'n',
 		'<leader>rn',
 		vim.lsp.buf.rename,
-		{ noremap = true, silent = true, buffer = true }
+		{ noremap = true, silent = true, buffer = true, desc = 'Rename' }
 	)
 
 	vim.keymap.set(
 		'n',
 		'gD',
 		vim.lsp.buf.declaration,
-		{ noremap = true, silent = true, buffer = true }
+		{
+			noremap = true,
+			silent = true,
+			buffer = true,
+			desc = 'Go to declaration',
+		}
 	)
 
 	vim.keymap.set(
 		'n',
 		'<C-]>',
 		function()
-			if (tscpOk) then
+			if tscpOk then
 				tscp.lsp_definitions()
 			else
 				vim.lsp.buf.definition()
 			end
 		end,
-		{ noremap = true, silent = true, buffer = true }
+		{
+			noremap = true,
+			silent = true,
+			buffer = true,
+			desc = 'Go to definition',
+		}
 	)
 
-	vim.keymap.set('n', ']r', function()
-		if tscpOk then
-			tscp.lsp_references()
-		else
-			vim.lsp.buf.references()
-		end
-	end, { noremap = true, silent = true, buffer = true })
+	vim.keymap.set(
+		'n',
+		']r',
+		function()
+			if tscpOk then
+				tscp.lsp_references()
+			else
+				vim.lsp.buf.references()
+			end
+		end,
+		{
+			noremap = true,
+			silent = true,
+			buffer = true,
+			desc = 'Go to references',
+		}
+	)
 
-	vim.keymap.set('n', '<leader>sym', function()
-		if tscpOk then
-			tscp.lsp_document_symbols()
-		else
-			vim.lsp.buf.document_symbol()
-		end
-	end, { noremap = true, silent = true, buffer = true })
+	vim.keymap.set(
+		'n',
+		'<leader>sym',
+		function()
+			if tscpOk then
+				tscp.lsp_document_symbols()
+			else
+				vim.lsp.buf.document_symbol()
+			end
+		end,
+		{
+			noremap = true,
+			silent = true,
+			buffer = true,
+			desc = 'Document symbols',
+		}
+	)
 
-	vim.keymap.set('n', '<leader>Sym', function()
-		if tscpOk then
-			tscp.lsp_dynamic_workspace_symbols()
-		else
-			vim.lsp.buf.workspace_symbol()
-		end
-	end, { noremap = true, silent = true, buffer = true })
+	vim.keymap.set(
+		'n',
+		'<leader>Sym',
+		function()
+			if tscpOk then
+				tscp.lsp_dynamic_workspace_symbols()
+			else
+				vim.lsp.buf.workspace_symbol()
+			end
+		end,
+		{
+			noremap = true,
+			silent = true,
+			buffer = true,
+			desc = 'Workspace symbols',
+		}
+	)
 end
 
 return P
