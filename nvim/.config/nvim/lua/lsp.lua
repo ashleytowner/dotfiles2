b680fecdf5b726826c8d1bd4bf3a6ef2227fb5e0
Ashley Towner <git@ashtowner.com>
2023-12-25 00:22:33 +1030

    nvim: Moved LSP config into one file

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -1,8 +1,6 @@
-local P = {}
-
 local tscpOk, tscp = pcall(require, 'telescope.builtin')
 
-function P.set_keymaps()
+local function set_keymaps()
 	vim.keymap.set(
 		{ 'n', 'i' },
 		'<C-k>',
@@ -233,4 +231,98 @@ function P.set_keymaps()
 	)
 end
 
-return P
+
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, "lua/?.lua")
+table.insert(runtime_path, "lua/?/init.lua")
+
+local masonLspOk, masonLsp = pcall(require, 'mason-lspconfig');
+local lspConfigOk, lspConfig = pcall(require, 'lspconfig');
+
+if not (masonLspOk or lspConfigOk) then
+	return
+end
+
+masonLsp.setup({
+	ensure_installed = {
+		'bashls',
+		'clangd',
+		'cssls',
+		'html',
+		'lua_ls',
+		'pyright',
+		'tsserver',
+		'vimls',
+	}
+})
+
+masonLsp.setup_handlers {
+	-- The first entry (without a key) will be the default handler
+	-- and will be called for each installed server that doesn't have
+	-- a dedicated handler.
+	function(server_name)
+		lspConfig[server_name].setup({
+			on_attach = function()
+				set_keymaps()
+			end,
+		})
+	end,
+	['tsserver'] = function(server_name)
+		lspConfig[server_name].setup({
+			on_attach = function(client)
+				client.server_capabilities.documentFormattingProvider = false
+				set_keymaps()
+			end
+		})
+	end,
+	['lua_ls'] = function(server_name)
+		lspConfig[server_name].setup({
+			on_attach = function()
+				set_keymaps()
+			end,
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { 'vim' }
+					},
+					workspace = {
+						library = {
+							[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+							[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true
+						}
+					}
+				}
+			}
+		})
+	end
+}
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+	vim.lsp.diagnostic.on_publish_diagnostics,
+	{
+		-- Enable underline, use default values
+		underline = true,
+		-- Enable virtual text, override spacing to 4
+		virtual_text = {
+			spacing = 1,
+		},
+		update_in_insert = false,
+	}
+)
+
+vim.fn.sign_define(
+	"DiagnosticSignError",
+	{ text = "", texthl = "DiagnosticSignError" }
+)
+vim.fn.sign_define(
+	"DiagnosticSignWarn",
+	{ text = "", texthl = "DiagnosticSignWarn" }
+)
+vim.fn.sign_define(
+	"DiagnosticSignInformation",
+	{ text = "", texthl = "DiagnosticSignInformation" }
+)
+vim.fn.sign_define(
+	"DiagnosticSignHint",
+	{ text = "", texthl = "DiagnosticSignHint" }
+)
