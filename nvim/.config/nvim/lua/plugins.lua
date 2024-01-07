7e08645a6aceef6790ac76cf8214a29c0615d6bf
Ashley Towner <git@ashtowner.com>
2024-01-07 18:58:45 +1030

    nvim: Changed default file sorter to fzy algorithm in telescope

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -53,7 +53,7 @@ local plugins = {
 							mirror = false,
 						},
 					},
-					file_sorter = require('telescope.sorters').get_fuzzy_file,
+					file_sorter = require('telescope.sorters').get_fzy_sorter,
 					file_ignore_patterns = {},
 					generic_sorter = require('telescope.sorters').get_generic_fuzzy_sorter,
 					winblend = 0,
