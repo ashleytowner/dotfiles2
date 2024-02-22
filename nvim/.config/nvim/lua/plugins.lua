f6028cd45a7575d1debe620e224fde1d3cee99e5
Ashley Towner <git@ashtowner.com>
2024-02-22 16:24:17 +1030

    nvim: Switched to copilot

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -512,7 +512,42 @@ local plugins = {
 	},
 	-- AI
 	{
+		'github/copilot.vim',
+		config = function()
+			vim.g.copilot_no_tab_map = true
+			vim.g.ai = 'copilot'
+
+			vim.keymap.set(
+				'i',
+				'<C-g>n',
+				'<Plug>(copilot-next)',
+				{ noremap = true }
+			)
+
+			vim.keymap.set(
+				'i',
+				'<C-g>p',
+				'<Plug>(copilot-prev)',
+				{ noremap = true }
+			)
+
+			vim.keymap.set(
+				'i',
+				'<Plug>(vimrc:copilot-dummy-map)',
+				'copilot#Accept("")',
+				{
+					noremap = true,
+					silent = true,
+					expr = true,
+					script = true,
+					desc = "Copilot dummy map"
+				}
+			)
+		end
+	},
+	{
 		'Exafunction/codeium.vim',
+		enabled = false,
 		cond = function()
 			return os.getenv('NO_AI') ~= 'true'
 		end,
